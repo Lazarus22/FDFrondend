934509b15447581ecfe62df67b0b66fbccd9cd27
Moss <moss.makes@gmail.com>
2023-08-28 14:05:49 -0400

    initial commit frontend

---
+++ flavor-visualizer/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FlavorVisualizer from './FlavorVisualizer'; // Adjust the path to match the location of FlavorVisualizer.js
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the Flavor Visualizer</h1>
+      <FlavorVisualizer />
+    </div>
+  );
+};
+
+export default App;
