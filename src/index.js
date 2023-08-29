574211000b05619c46178886afa48f2e33c20ece
Moss <moss.makes@gmail.com>
2023-08-29 12:26:49 -0400

    ssl redirect

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+// Enforce HTTPS
+if (window.location.protocol !== 'https:') {
+  window.location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+}
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
