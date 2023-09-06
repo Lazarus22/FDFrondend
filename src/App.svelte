2487db932fe06001871a6ec7ec5111c3f3612c00
Moss <moss.makes@gmail.com>
2023-09-05 21:32:41 -0400

    mvp

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,18 @@
 <script>
-	export let name;
+  import ForceDirectedGraph from './components/ForceDirectedGraph.svelte';
+  import { flavors } from './stores';
+  
+  let searchInput = '';
+
+  function handleSearch() {
+    flavors.set([searchInput]);
+    console.log(`Set flavor in store: ${searchInput}`);
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div>
+  <input type="text" bind:value={searchInput} placeholder="Search for a flavor..." />
+  <button on:click={handleSearch}>Search</button>
+</div>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<ForceDirectedGraph />
---
+++ src/components/ForceDirectedGraph.svelte
@@ -0,0 +1,81 @@
+<script>
+  import * as d3 from 'd3';
+  import { flavors } from '../stores';
+
+  let height;
+
+  $: flavors.subscribe(async currentFlavors => {
+    if (currentFlavors && currentFlavors.length > 0) {
+      const flavor = currentFlavors[0];
+      await fetchData(flavor);
+    }
+  });
+
+  async function fetchData(flavor) {
+    try {
+      const res = await fetch(`https://fdbackend-d0a756cc3435.herokuapp.com/recommendations?flavor=${flavor}`);
+      const data = await res.json();
+      if (data && data.recommendations) {
+        renderVisualization(data);
+      }
+    } catch (error) {
+      console.log('Error fetching data:', error);
+    }
+  }
+
+  function renderVisualization(data) {
+    const flavor = data.flavor;
+    const links = data.recommendations.map(recommendation => ({ source: flavor, target: recommendation, value: 1 }));
+    height = window.innerHeight - 50;
+    const nodes = Array.from(new Set(links.flatMap(l => [l.source, l.target])), id => ({ id }));
+    const dynamicLinkDistance = Math.sqrt(nodes.length) * 5;
+    const dynamicCharge = -Math.sqrt(nodes.length) * 5;
+
+    const svg = d3.select('#visualization').html('')
+                  .append('svg').attr('width', '100%').attr('height', height);
+
+    const container = svg.append('g');
+    const width = window.innerWidth;
+
+    // Add zoom functionality
+    svg.call(d3.zoom().on('zoom', (event) => {
+      container.attr('transform', event.transform);
+    }));
+
+    const simulation = d3.forceSimulation(nodes)
+                          .force('link', d3.forceLink(links).id(d => d.id).distance(dynamicLinkDistance))
+                          .force('charge', d3.forceManyBody().strength(dynamicCharge))
+                          .force('center', d3.forceCenter(width / 2, height / 2));
+
+    const link = container.append('g').attr('stroke', '#999').attr('stroke-opacity', 0.6)
+                  .selectAll('line').data(links).join('line')
+                  .attr('stroke-width', d => Math.sqrt(d.value));
+
+    const node = container.append('g').attr('stroke', '#fff').attr('stroke-width', 1.5)
+                  .selectAll('g').data(nodes).join('g')
+                  .call(d3.drag().on('start', dragstarted).on('drag', dragged));
+
+    node.append('circle').attr('r', 5).attr('fill', '#6699cc');
+    node.append('text').attr('stroke', 'none').attr('fill', '#000')
+        .attr('text-anchor', 'middle').attr('dy', '0.35em').text(d => d.id);
+
+    simulation.on('tick', () => {
+      link.attr('x1', d => d.source.x).attr('y1', d => d.source.y)
+          .attr('x2', d => d.target.x).attr('y2', d => d.target.y);
+      node.attr('transform', d => `translate(${d.x}, ${d.y})`);
+    });
+
+    function dragstarted(event, d) {
+      if (!event.active) simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+
+    function dragged(event, d) {
+      d.fx = event.x;
+      d.fy = event.y;
+    }
+  }
+</script>
+
+<div id="visualization"></div>
