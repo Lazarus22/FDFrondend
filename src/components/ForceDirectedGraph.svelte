d230cce59ba54fa288e4579b761a841372ce926b
Moss <moss.makes@gmail.com>
2023-09-14 16:24:15 -0400

    test

--- src/components/ForceDirectedGraph.svelte
+++ src/components/ForceDirectedGraph.svelte
@@ -119,23 +119,7 @@
       .attr("width", width)
       .attr("height", height);
 
-    const zoomGroup = svg.append("g"); // Define zoomGroup after svg
-
-    const colorScale = d3.scaleLinear().domain([1, 4]).range(["#ccc", "#000"]); // Define colorScale
-
-    const simulation = d3
-      .forceSimulation(nodes)
-      .force(
-        "link",
-        d3
-          .forceLink(links)
-          .id((d) => d.name)
-          .distance(100)
-      )
-      .force("charge", d3.forceManyBody().strength(-500))
-      .force("center", d3.forceCenter(width / 2, height / 2));
-
-      nodes.push({ name: rec.name, nodeType: rec.nodeType, x: clickedNode.x, y: clickedNode.y });
+    const zoomGroup = svg.append("g");
 
     const link = zoomGroup
       .append("g")
@@ -143,7 +127,6 @@
       .data(links)
       .enter()
       .append("line")
-      .attr("stroke", (d) => colorScale(d.strength))
       .attr("stroke-width", 0.5);
 
     const nodeGroup = zoomGroup
@@ -170,50 +153,6 @@
       .attr("font-size", "12px")
       .attr("font-family", "Arial, Helvetica, sans-serif")
       .attr("pointer-events", "none");
-
-    const zoom = d3
-      .zoom()
-      .scaleExtent([0.1, 10])
-      .on("zoom", (event) => {
-        zoomGroup.attr("transform", event.transform);
-      });
-
-    // Drag functions
-    function dragstarted(event, d) {
-      if (!event.active) simulation.alphaTarget(0.3).restart();
-      d.fx = d.x;
-      d.fy = d.y;
-    }
-
-    function dragged(event, d) {
-      d.fx = event.x;
-      d.fy = event.y;
-    }
-
-    function dragended(event, d) {
-      if (!event.active) simulation.alphaTarget(0);
-      d.fx = null;
-      d.fy = null;
-    }
-
-    // Add drag behavior to nodes
-    const drag = d3
-      .drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended);
-
-    nodeGroup.call(drag);
-
-    svg.call(zoom);
-
-    simulation.on("tick", () => {
-      link
-        .attr("x1", (d) => d.source.x)
-        .attr("y1", (d) => d.source.y)
-        .attr("x2", (d) => d.target.x)
-        .attr("y2", (d) => d.target.y);
-    });
   }
 
   onMount(() => fetchDataAndUpdate(flavor));
