58ea9dba0dfa88d7135bf1841d62a3fd7ca0f911
Moss <moss.makes@gmail.com>
2023-09-14 16:26:58 -0400

    test

--- src/components/ForceDirectedGraph.svelte
+++ src/components/ForceDirectedGraph.svelte
@@ -13,11 +13,6 @@
     );
     const data = await res.json();
 
-    // Check if recommendations are null
-    if (data.recommendations === null) {
-      return; // Exit the function early
-    }
-
     if (!nodes.some((node) => node.name === data.flavor)) {
       nodes.push({ name: data.flavor, nodeType: "Flavor" });
     }
@@ -42,7 +37,6 @@
       }
     });
   }
-
   function collapseNode(flavor) {
     // Remove the node from the expandedNodes set
     expandedNodes.delete(flavor);
@@ -88,17 +82,6 @@
     });
   }
 
-  const colorMap = {
-    Ingredient: "#fee07e",
-    Taste: "#ecb5ca",
-    Volume: "#f16767",
-    Weight: "#ca90c0",
-    Season: "#8cce91",
-    Function: "#f79767",
-    Technique: "#58c7e3",
-    Related: "#d9c8ae",
-  };
-
   async function fetchDataAndUpdate(flavor) {
     if (!expandedNodes.has(flavor)) {
       await expandNode(flavor);
@@ -119,7 +102,21 @@
       .attr("width", width)
       .attr("height", height);
 
-    const zoomGroup = svg.append("g");
+    const zoomGroup = svg.append("g"); // Define zoomGroup after svg
+
+    const colorScale = d3.scaleLinear().domain([1, 4]).range(["#ccc", "#000"]); // Define colorScale
+
+    const simulation = d3
+      .forceSimulation(nodes)
+      .force(
+        "link",
+        d3
+          .forceLink(links)
+          .id((d) => d.name)
+          .distance(100)
+      )
+      .force("charge", d3.forceManyBody().strength(-500))
+      .force("center", d3.forceCenter(width / 2, height / 2));
 
     const link = zoomGroup
       .append("g")
@@ -127,7 +124,8 @@
       .data(links)
       .enter()
       .append("line")
-      .attr("stroke-width", 0.5);
+      .attr("stroke", (d) => colorScale(d.strength))
+      .attr("stroke-width", 1);
 
     const nodeGroup = zoomGroup
       .append("g")
@@ -140,10 +138,7 @@
         fetchDataAndUpdate(d.name);
       });
 
-    nodeGroup
-      .append("circle")
-      .attr("r", 5)
-      .attr("fill", (d) => colorMap[d.nodeType] || "#fee07e");
+    nodeGroup.append("circle").attr("r", 5).attr("fill", "#69b3a2");
 
     nodeGroup
       .append("text")
@@ -153,6 +148,52 @@
       .attr("font-size", "12px")
       .attr("font-family", "Arial, Helvetica, sans-serif")
       .attr("pointer-events", "none");
+
+    const zoom = d3
+      .zoom()
+      .scaleExtent([0.1, 10])
+      .on("zoom", (event) => {
+        zoomGroup.attr("transform", event.transform);
+      });
+
+    // Drag functions
+    function dragstarted(event, d) {
+      if (!event.active) simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+
+    function dragged(event, d) {
+      d.fx = event.x;
+      d.fy = event.y;
+    }
+
+    function dragended(event, d) {
+      if (!event.active) simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+    }
+
+    // Add drag behavior to nodes
+    const drag = d3
+      .drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended);
+
+    nodeGroup.call(drag);
+
+    svg.call(zoom);
+
+    simulation.on("tick", () => {
+      link
+        .attr("x1", (d) => d.source.x)
+        .attr("y1", (d) => d.source.y)
+        .attr("x2", (d) => d.target.x)
+        .attr("y2", (d) => d.target.y);
+
+      nodeGroup.attr("transform", (d) => `translate(${d.x}, ${d.y})`);
+    });
   }
 
   onMount(() => fetchDataAndUpdate(flavor));
