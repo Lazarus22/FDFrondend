ec7becffadb5505e3246ce2e596408a2f050235d
Moss <moss.makes@gmail.com>
2023-09-15 07:54:42 -0400

    test

--- src/components/ForceDirectedGraph.svelte
+++ src/components/ForceDirectedGraph.svelte
@@ -127,17 +127,25 @@
 
     const colorScale = d3.scaleLinear().domain([1, 4]).range(["#ccc", "#000"]); // Define colorScale
 
-    const simulation = d3
-      .forceSimulation(nodes)
-      .force(
-        "link",
-        d3
-          .forceLink(links)
-          .id((d) => d.name)
-          .distance(100)
-      )
-      .force("charge", d3.forceManyBody().strength(-500))
-      .force("center", d3.forceCenter(width / 2, height / 2));
+    const simulation = d3.forceSimulation(nodes)
+  .force("link", d3.forceLink(links).id(d => d.id))
+  .force("charge", d3.forceManyBody().strength(-400))
+  .force("center", d3.forceCenter(width / 2, height / 2))
+  .force("collide", d3.forceCollide(50));
+
+// Event listeners for simulation updates (tick)
+simulation.on("tick", () => {
+  // Update your nodes and links here
+  // For example, if you're using SVG circles for nodes:
+  node.attr("cx", d => d.x)
+      .attr("cy", d => d.y);
+
+  // And if you're using SVG lines for links:
+  link.attr("x1", d => d.source.x)
+      .attr("y1", d => d.source.y)
+      .attr("x2", d => d.target.x)
+      .attr("y2", d => d.target.y);
+});
 
     const link = zoomGroup
       .append("g")
