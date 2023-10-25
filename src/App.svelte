c9120d9796ebc0c3451a526c70bc1051d01d754a
Moss <moss.makes@gmail.com>
2023-10-24 20:36:47 -0400

    test

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import ForceDirectedGraph from './components/ForceDirectedGraph.svelte';
-  import Analytics from './components/Analytics.svelte'; 
+  import Analytics from './components/Analytics.svelte';
+  import { load } from '@sveltejs/kit/http';
 </script>
 
 <Analytics />
@@ -10,10 +11,24 @@
 </main>
 
 <style>
-main {
-  text-align: center;
-  padding: 0;
-  width: 100%;
-  margin: 0;
-}
+  main {
+    text-align: center;
+    padding: 0;
+    width: 100%;
+    margin: 0;
+  }
 </style>
+
+<!-- HTTPS enforcement middleware -->
+<script context="module">
+  export async function beforeLoad({ request }) {
+    // Check if the request protocol is not HTTPS
+    if (request.protocol !== 'https') {
+      // Redirect to HTTPS
+      const httpsUrl = `https://${request.host}${request.path}`;
+      return load(httpsUrl, request);
+    }
+    // Continue with the request
+    return load(request);
+  }
+</script>
