ce75f4ac25278c9fe1a520aee0357683a2bd11fd
Moss <moss.makes@gmail.com>
2023-10-24 21:14:11 -0400

    test2

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
+// Add this block for HTTPS redirection
+if (window.location.protocol === 'http:') {
+  window.location.href = window.location.href.replace('http:', 'https:');
+}
+
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  target: document.body,
+  props: {
+    name: 'world'
+  }
 });
 
 export default app;
