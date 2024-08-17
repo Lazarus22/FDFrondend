6fc27c60b969f65056bd84c719c70105652e3fe8
laszarus22 <mathew.peters.nyc@gmail.com>
2024-08-16 20:12:21 -0400

    asdfashga

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,12 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default defineConfig({
-  plugins: [sveltekit()]
-});
+const config = {
+  extensions: ['.svelte'],
+  preprocess: [vitePreprocess()],
+  kit: {
+    adapter: adapter()
+  }
+};
+
+export default config;
